8260ebfad1be7c78650218914e65fc7a964ad5d6
James-tech2 <joseph659283@outlook.com>
2026-06-01 10:10:00 -0400

    initial setup

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+
+export default [
+  js.configs.recommended,
+  {
+    plugins: {
+      react,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
