2b5f51f8d0e3f640bdcdc042ed68b0efe12e5dbe
James-tech2 <joseph659283@outlook.com>
2026-06-07 08:56:43 -0400

    fix test error

--- src/test/About.test.jsx
+++ src/test/About.test.jsx
@@ -7,5 +7,6 @@ describe('About', () => {
 
     expect(screen.getByText(/About Me/i)).toBeInTheDocument();
     expect(screen.getByText(/Hi, I’m James Black\. I build modern full-stack web experiences\./i)).toBeInTheDocument();
+    expect(screen.getByAltText(/Portrait of James Black/i)).toBeInTheDocument();
   });
 });
